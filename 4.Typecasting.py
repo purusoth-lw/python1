ca3ad18e53d651da8f97c208f403d1a2d1f0ebb7
brooze <56531358+purusoth-lw@users.noreply.github.com>
2021-05-06 18:01:50 +0530

    Add files via upload

---
+++ 4.Typecasting.py
@@ -0,0 +1,18 @@
+"""
+Typecasting:
+============
+convert the data in one type another.
+
+syntax:
+=======
+datatype(variable)
+"""
+
+a=1.2
+print(type(a))
+print("a : ",a)
+b=int(a)
+print(type(b))
+print("b : ",b)
+
+
